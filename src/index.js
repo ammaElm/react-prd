b182618db89de7c41b66446bf03e65b88a2f339c
amaris <amarisyu@sina.com>
2018-11-08 17:25:34 +0800

    all

--- src/index.js
+++ src/index.js
@@ -1,10 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import FastClick from 'fastclick';
+import Route from './router/index.js';
+// import Home from './pages/home/home'
 import './index.css';
-import App from './App';
+// import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { AppContainer } from 'react-hot-loader'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+FastClick.attach(document.body)
+
+const render = Component =>{
+  ReactDOM.render(  
+    <AppContainer>
+      <Component />
+    </AppContainer>,
+      document.getElementById('root')
+  )
+}
+render(Route)
+
+if (module.hot) {
+  module.hot.accept('./router/index.js', () => {
+    render(Route);
+  })
+}
+// ReactDOM.render(<Route />, document.getElementById('root'));
+
+// ReactDOM.render(<Home />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
---
+++ src/pages/home/home.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+
+
+class Home extends Component{
+  // constructor(){
+
+  // }
+  
+  render() {
+    return (
+      <h1>hhq3451sh</h1>
+    )
+  }
+
+}
+
+export default Home
