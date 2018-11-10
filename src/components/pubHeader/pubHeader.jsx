a4d1b3f6b12b5c9dd9c37f2ea6cdba843f800bca
amaris <amarisyu@sina.com>
2018-11-10 17:10:06 +0800

    all

---
+++ src/components/pubHeader/pubHeader.jsx
@@ -0,0 +1,18 @@
+import './header.less'
+import React, { Component } from 'react'
+
+
+class pubHeader extends Component{
+
+  render(){
+    return (
+      <div>
+        
+
+      </div>
+    )
+  }
+
+}
+
+export default pubHeader
--- src/utils/setRem.js
+++ src/utils/setRem.js
@@ -2,12 +2,14 @@
   let remSize = 0
   let scale = 0
   let dpr = window.devicePixelRatio
+  console.log(dpr)
   // let domEle = document.documentElement
   let currentWidth = document.documentElement.clientWidth
   scale = currentWidth/psdw
   remSize = psdw/10
   remSize = remSize*scale
   document.documentElement.fontSize = remSize + 'px'
+  console.log(document.documentElement.fontSize)
   document.documentElement.setAttribute('data-dpr',dpr)
-})(750)
+})(1500)
 
